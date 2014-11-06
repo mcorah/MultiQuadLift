85fd7f3dc760ee73d6456bd5b469e383ecd82b6b
Micah Corah <micahcorah@gmail.com>
2014-11-05 19:40:52 -0500

    julia version not working, suspecting numerical integration

--- main.jl
+++ main.jl
@@ -1,25 +1,61 @@
 using PyPlot
 using Sundials
+import Base.show
 
-tmax = 1e2;
-dt = 1e-3;
-d = 0.1
-init = [1.0,1.0,3.0,0.0];
+tmax = 1e0
+dt = 1e-6
+init = [0.2,0.2,0.1,0.1,0.0,0.0,0.0,0.0,0.0,0.0]
+
+I = 1.0
+M = 1.0
+g = 9.8
+wn_rp = 9.0
+wn_z = 5.0
+wn_xy = 2.0
+Kp=M * diagm([wn_xy^2,wn_xy^2,wn_z^2])
+Kdp=M * diagm([wn_xy,wn_xy,wn_z])
+Ka=I * diagm([wn_rp^2,wn_rp^2])
+Kda=I * diagm([wn_rp,wn_rp])
+
+A = [
+     zeros(3,3) eye(3) zeros(3,4);
+     zeros(2,6) [0 g; -g 0] zeros(2,2);
+     zeros(1,10);
+     zeros(2,8) eye(2);
+     zeros(2,10)
+    ]
+B = [
+     zeros(5,3);
+     1/M 0 0;
+     zeros(2,3);
+     0 1/I 0;
+     0 0 1/I;
+    ]
+
+#show(A)
 
 function f(t, yin, ydot)
-  x = yin[1];
-  y = yin[2];
-  z = yin[3];
-  dz = yin[4];
-  ydot[1] = y;
-  ydot[2] = -x;
-  ydot[3] = dz;
-  ydot[4] = -z - d*dz;
-end
+  p = yin[1:3]
+  dp = yin[4:6]
+  a = yin[7:8]
+  da = yin[9:10]
+  acc_des = M .* (Kp*(-p) + Kdp*(-dp));
+  att_des = 1/g .* [0 -1.0 0; 1.0 0 0] * acc_des
+  u = [
+       M * (acc_des[3]);
+       Ka * (att_des-a) + Kda * (-da)
+      ]
+  #show(u)
+  #print('\n')
+  ydot = A*yin + B*u
+  #show(ydot)
+  print('\n')
+end;
 
-t = [0:dt:tmax];
+t = [0:dt:tmax]
 
-pos = Sundials.cvode(f, init, t);
+pos = Sundials.cvode(f, init, t)
 
-figure();
-plot3D(pos[:,1],pos[:,2],pos[:,3]);
+figure()
+#show(pos)
+plot3D(pos[:,1],pos[:,2],pos[:,3])
