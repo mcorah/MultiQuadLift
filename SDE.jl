a00e778ac1e5447f8735d73a5a0bb9ab7825e19d
Micah Corah <micahcorah@gmail.com>
2014-11-14 12:04:43 -0500

    fixing indentation

--- SDE.jl
+++ SDE.jl
@@ -1,43 +1,43 @@
 module SDE
-  #=
-  Euler-Maruyama method
-  dX(t) = f(X(t))dt + g(X(t))dW(t)
-  t0 <= t <= tf
-  Brownian steps occur at a rate dt
-  R Brownian steps per step of integration
+#=
+Euler-Maruyama method
+dX(t) = f(X(t))dt + g(X(t))dW(t)
+t0 <= t <= tf
+Brownian steps occur at a rate dt
+R Brownian steps per step of integration
 
-  Based on examples from:
-  An Algorithmic Introduction to Numerical Simulation of Stochastic Differential
-  Equations by Desmond Higham
-  =#
-  function em(f, g, X0, tf, dt, R)
-    T = [0:dt:tf]'
+Based on examples from:
+An Algorithmic Introduction to Numerical Simulation of Stochastic Differential
+Equations by Desmond Higham
+=#
+function em(f, g, X0, tf, dt, R)
+  T = [0:dt:tf]'
 
-    N = length(T)
+  N = length(T)
 
-    #The number of random variables is not given explicitly
-    nW = size(g(X0),2)
+  #The number of random variables is not given explicitly
+  nW = size(g(X0),2)
 
-    dW = sqrt(dt) * randn(nW,N)
+  dW = sqrt(dt) * randn(nW,N)
 
-    L = N/R
-    Dt = R*dt
+  L = N/R
+  Dt = R*dt
 
-    Xs = zeros(length(X0),N)
-    Xs[:,1] = X0
+  Xs = zeros(length(X0),N)
+  Xs[:,1] = X0
 
-    Wi = zeros(size(dW,1),1)
-    for i = 2:L
-      Wi[:] = mapslices(sum, dW[:, R*(i-1)+1:R*i], 2)
-      Xs[:,i] = Xs[:,i-1] + f(Xs[:,i-1])*Dt + g(Xs[:,i-1])*Wi
-    end
-
-    return Xs,T
+  Wi = zeros(size(dW,1),1)
+  for i = 2:L
+    Wi[:] = mapslices(sum, dW[:, R*(i-1)+1:R*i], 2)
+    Xs[:,i] = Xs[:,i-1] + f(Xs[:,i-1])*Dt + g(Xs[:,i-1])*Wi
   end
 
-  type Model
-    f::Function
-    g::Function
-    Model(fn::Matrix{Float64}, gn::Matrix{Float64}) = new(x -> fn*x, x -> gn)
-  end
+  return Xs,T
+end
+
+type Model
+  f::Function
+  g::Function
+  Model(fn::Matrix{Float64}, gn::Matrix{Float64}) = new(x -> fn*x, x -> gn)
+end
 end
