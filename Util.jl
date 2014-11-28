f338c22c8dd3498178e2e86c7839cd19fe74660c
Micah Corah <micahcorah@gmail.com>
2014-11-27 21:35:52 -0500

    updates to Util and adding index_dim to Util

--- Util.jl
+++ Util.jl
@@ -1,10 +1,14 @@
 module Util
-  block_diag(a) = a
-  function block_diag(a,b,c...)
-    block_ab = [a zeros(size(a,1), size(b,2));
-                zeros(size(b,1), size(a,2)) b]
-    block_diag(block_ab,c...)
-  end
 
-  export block_diag
+export block_diag, indexDim
+
+block_diag(a) = a
+function block_diag(a,b,c...)
+  block_ab = [a zeros(size(a,1), size(b,2));
+              zeros(size(b,1), size(a,2)) b]
+  block_diag(block_ab,c...)
+end
+
+index_dim(coord, dim) = dot((coord-1), cumprod([1;dim][1:end-1])) + 1
+
 end
