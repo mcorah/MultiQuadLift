eccf74d5c706bc5e0f22c5f24a363533669a4e3e
Micah Corah <micahcorah@gmail.com>
2014-11-27 22:34:30 -0500

    test scripts

---
+++ multiple_quadrotor.jl
@@ -0,0 +1,36 @@
+using Quadrotor
+using PyPlot
+using SDE
+
+tmax = 1e1
+dt = 4e-4
+R = 1
+
+I = 1.0
+M = 1.0
+wn_rp = 9.0
+wn_z = 5.0
+wn_xy = 2.0
+mu = 0.1
+
+dims = [2,2,2]
+dist = 0.5
+
+mass_params = MassParams(M,I)
+
+gains = critically_damped(mass_params, wn_xy, wn_z, wn_rp)
+
+quad_params = QuadrotorParams(mass_params, gains, mu)
+
+multi_params = MultiAgentParams(dims, dist, quad_params)
+
+system,init = create_multi_agent_system(multi_params)
+
+@time X,T = SDE.em(system, init, tmax, dt, R, sample_rate = 100)
+
+figure()
+len = size(Quadrotor.linear_dynamics(),1)
+for i=1:prod(dims)
+  temp = X[1+(i-1)*len:3+(i-1)*len, :]'
+  plot3D(temp[:,1],temp[:,2],temp[:,3])
+end
---
+++ test_quadrotor.jl
@@ -0,0 +1,52 @@
+using DynamicSystem
+using Quadrotor
+using SDE
+using PyPlot
+s = System()
+arr = SystemArray(s)
+set_specification(s,arr)
+
+for i=1:2
+  for j=1:2
+    for k=1:2
+      temp_system = System()
+      temp_system.parent = s
+
+      q = QuadrotorSpecification()
+      mass_params = MassParams(1,1)
+      gains = critically_damped(mass_params, 2, 5, 9)
+      quad_params = QuadrotorParams(mass_params, gains, 0.1)
+      q.params = quad_params
+      q.desired_position = 1.0 * [i,j,k]
+
+      controller = QuadrotorController(GlobalEstimator())
+      q.traits = [controller]
+
+      set_specification(temp_system,q)
+      push(arr,temp_system)
+    end
+  end
+end
+  
+dyn = system_dynamics(s)
+noise = noise_dynamics(s)
+dyn = [dyn; spzeros(1,size(dyn,2))]
+noise = [noise; spzeros(1,size(noise,2))]
+println("dynamics")
+show(dyn); println()
+println("noise")
+show(noise)
+
+init = [init_vals(s),1]
+init[end] = 1
+system = SDE.Model(dyn, noise)
+
+@time X,T = SDE.em(system, init, 2e1, 1e-4, 1, sample_rate = 100)
+
+figure()
+poss = get_states(pos_states, s, X)
+
+for i = 1:size(poss,3)
+  temp = poss[:,:,i]'
+  plot3D(temp[:,1],temp[:,2],temp[:,3])
+end
