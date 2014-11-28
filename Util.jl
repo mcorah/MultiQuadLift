06bc1b77b783fbd559936d4d7acb0aed1dc8cfd5
Micah Corah <micahcorah@gmail.com>
2014-11-28 14:57:25 -0500

    adding cross product function

--- Util.jl
+++ Util.jl
@@ -1,6 +1,6 @@
 module Util
 
-export block_diag, index_dim
+export block_diag, index_dim, cross_matrix
 
 block_diag(a) = a
 function block_diag(a,b,c...)
@@ -10,5 +10,7 @@ function block_diag(a,b,c...)
 end
 
 index_dim(coord, dim) = dot((coord-1), cumprod([1;dim][1:end-1])) + 1
+
+cross_matrix(v::Vector) = [0 -v[3] v[2]; v[3] 0 -v[1]; -v[2] v[1] 0]
 
 end
