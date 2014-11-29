a3793a21b98b0216dff55f0e02fcf5b663bbba25
Micah Corah <micahcorah@gmail.com>
2014-11-29 00:43:47 -0500

    payload test file

---
+++ payload_system.jl
@@ -0,0 +1,49 @@
+using Quadrotor
+using PyPlot
+using SDE
+
+tmax = 1e1
+dt = 2e-4
+R = 1
+
+I = 1.0
+M = 1.0
+wn_rp = 9.0
+wn_z = 5.0
+wn_xy = 2.0
+mu = 0.1
+
+dims = [4,4,8]
+dist = 1
+
+spring_constant = 1e2
+weight_fraction = 0.7
+
+mass_params = MassParams(M,I)
+
+gains = critically_damped(mass_params, wn_xy, wn_z, wn_rp)
+
+quad_params = QuadrotorParams(mass_params, gains, mu)
+
+multi_params = MultiAgentParams(dims, dist, quad_params; relative=true)
+
+payload_params = PayloadSystemParams(weight_fraction, spring_constant,
+                                     multi_params)
+
+system = create_payload_system(payload_params)
+
+system_matrix = system_dynamics(system)
+noise_matrix = system_noise(system)
+init = init_vals(system)
+
+model = SDE.Model(system_matrix, noise_matrix)
+
+@time X,T = SDE.em(model, init, tmax, dt, R, sample_rate = 100)
+
+figure()
+poss = get_states(pos_states, system, X)
+
+for i = 1:size(poss,3)
+  temp = poss[:,:,i]'
+  plot3D(temp[:,1],temp[:,2],temp[:,3])
+end
