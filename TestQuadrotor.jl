69b80ada3d58cc87f06883bddf430c90e9e657c9
Micah Corah <micahcorah@gmail.com>
2014-12-02 12:32:40 -0500

    upadtes to test quadrotor

--- TestQuadrotor.jl
+++ TestQuadrotor.jl
@@ -1,31 +1,39 @@
 module TestQuadrotor
 
-using BatchTest
-using StandardQuadrotor
+importall BatchTest
+importall StandardQuadrotor
 
-export run_tests
+export run_tests, load_test, series, file_name
+export data_series, test_types
 
 # file_name
 # system_type dim dim dim.test
 data_series = {
   "1d_z" => [1, 1, 2].^([0:10]'),
   "1d_y" => [1, 2, 1].^([0:10]'),
-  "2d_z" => [2, 2, 1].^([0:8]'),
-  "2d_y" => [2, 1, 2].^([0:8]'),
+  "2d_z" => [2, 2, 1].^([0:6]'),
+  "2d_y" => [2, 1, 2].^([0:6]'),
   "3d" => [2, 2, 2].^([0:5]')
   }
 
 test_types = [multi_agent_system, relative_multi_agent_system, payload_system,
   relative_payload_system]
 
-file_name(system_fun, series, x, y, z) = "./test/$system_fun#$series#$x\_$y\_$z"
+function file_name(system_fun, series, index::Integer)
+  dim = data_series[series][:,index]
+  file_name(system_fun, series, dim)
+end
 
-function create_test(system_fun, series, index)
-  dim = data_series[series][:, index]
+function file_name(system_fun, series, dim::Vector)
   x = dim[1]
   y = dim[2]
   z = dim[3]
-  Test(file_name(system_fun, series, x, y, z),
+  "./test/$system_fun#$series#$x\_$y\_$z"
+end
+
+function create_test(system_fun, series, index)
+  dim = data_series[series][:, index]
+  Test(file_name(system_fun, series, dim),
     ()->run_test(system_fun, dim))
 end
 
@@ -40,6 +48,18 @@ function run_tests(overwrite = false)
   for test = tests
     execute_test(test, overwrite = overwrite)
   end
+end
+
+function series(system_fun, series_name)
+  tests = data_series[series_name]
+  [(tests[:,1], file_name(system_fun, series_name, tests[:,1])) for i = 1:size(tests,2)]
+end
+
+function load_test(fun::Integer, series::Integer, index::Integer)
+  series_names = collect(keys(data_series))
+  series_name = series_names[series]
+  name = file_name(test_types[fun], series_name, index)
+  load_test(name)
 end
 
 end
